8e0106b67ad1bfadfd028005e039d9fb0f978ad4
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-04-05 07:01:52 +0000

    [update]メソッドの簡略化

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,8 +1,13 @@
 def fizz_buzz(number)
-  return "Fizz Buzz" if number % 15 == 0
-  return "Fizz" if number % 3 ==0
-  return "Buzz" if number % 5 ==0
-  number.to_s
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s
+  end
 end
 
 
